b412fae3e50203fc3568140fac365a9259c9e59c
hkdnet <satoko.itse@gmail.com>
2019-04-26 03:30:43 +0900

    USAGE: action

--- src/main/scala/net/hkdnet/main.scala
+++ src/main/scala/net/hkdnet/main.scala
@@ -4,17 +4,24 @@ import net.hkdnet.numeric.Rational
 import net.hkdnet.numeric._
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    println(new Rational(1) / 4 * 3 + new Rational(1) / 2)
+  def main(args: Array[String]): Unit = args match {
+    case Array() => println("USAGE: action")
+    case Array("rational") => {
+      println(new Rational(1) / 4 * 3 + new Rational(1) / 2)
+    }
+    case Array("spiral") => {
 
-    // (x + y) * z + 1
-    val expr = BinOp(
-      "+",
-      BinOp("*",
-        BinOp("+", Var("x"), Var("y")),
-        Var("z")),
-      Number(1)
-    )
-    println(expr)
+    }
+    case Array("expr") => {
+      // (x + y) * z + 1
+      val expr = BinOp(
+        "+",
+        BinOp("*",
+          BinOp("+", Var("x"), Var("y")),
+          Var("z")),
+        Number(1)
+      )
+      println(expr)
+    }
   }
 }
