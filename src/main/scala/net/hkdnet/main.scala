6bb1c8355210097a39f97e72ed2f9c332e0b932e
hkdnet <satoko.itse@gmail.com>
2019-04-26 23:54:58 +0900

    spiral

--- src/main/scala/net/hkdnet/main.scala
+++ src/main/scala/net/hkdnet/main.scala
@@ -1,7 +1,28 @@
 package net.hkdnet
 
-import net.hkdnet.numeric.Rational
 import net.hkdnet.numeric._
+import net.hkdnet.layout._
+
+object Spiral {
+  val space = Element.elem(" ")
+  val corner = Element.elem("+")
+  def spiral(nEdges: Int, direction: Int): Element = {
+    if (nEdges == 1)
+      corner
+    else {
+      val sp = spiral(nEdges - 1, (direction + 3) % 4)
+      def verticalBar = Element.elem('|', 1, sp.height)
+      def horizontalBar = Element.elem('-', sp.width, 1)
+
+      direction match {
+        case 0 => (corner beside horizontalBar) above (sp beside space)
+        case 1 => (sp above space) beside (corner above verticalBar)
+        case 2 => (space beside sp) above (horizontalBar beside corner)
+        case _ => (verticalBar above corner) beside(space above sp)
+      }
+    }
+  }
+}
 
 object Main {
   def main(args: Array[String]): Unit = args match {
@@ -10,7 +31,9 @@ object Main {
       println(new Rational(1) / 4 * 3 + new Rational(1) / 2)
     }
     case Array("spiral") => {
-
+      println(
+        Spiral.spiral(14, 0)
+      )
     }
     case Array("expr") => {
       // (x + y) * z + 1
