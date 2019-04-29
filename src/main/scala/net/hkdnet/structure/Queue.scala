40493ab7dc0e417feff630ccb268c2ad5c3d405f
hkdnet <satoko.itse@gmail.com>
2019-04-30 01:15:04 +0900

    Make Queue co-varient

--- src/main/scala/net/hkdnet/structure/Queue.scala
+++ src/main/scala/net/hkdnet/structure/Queue.scala
@@ -1,38 +1,46 @@
 package net.hkdnet.structure
 
-trait Queue[T] {
+trait Queue[+T] {
   def head: T
+
   def tail: Queue[T]
-  def enqueue(e: T): Queue[T]
+
+  def enqueue[U >: T](e: U): Queue[U]
 }
 
 
 object Queue {
   def apply[T](xs: T*): Queue[T] = new QueueImpl[T](xs.toList)
 
-  private class QueueImpl[T] private (private val leading: List[T], private val trailing: List[T]) extends Queue[T] {
+  private class QueueImpl[+T] private(private[this] var leading: List[T],
+                                      private[this] var trailing: List[T]) extends Queue[T] {
     def this(list: List[T]) {
       this(list.take(list.size / 2), list.drop(list.size / 2).reverse)
     }
 
-    // TODO: rebalance...?
-    private def mirror: QueueImpl[T] = {
+    private def mirror() = {
       if (leading.isEmpty) {
-        new QueueImpl(trailing.reverse, Nil)
-      } else {
-        this
+        while (!trailing.isEmpty) {
+          leading = trailing.head :: leading
+          trailing = trailing.tail
+        }
       }
     }
 
-    def head: T = mirror.leading.head
-    def tail: Queue[T] = {
-      val q = mirror
-      new QueueImpl(q.leading.tail, q.trailing)
+    def head: T = {
+      leading.head
     }
-    def enqueue(e: T): Queue[T] = new QueueImpl[T](leading, e::trailing)
+
+    def tail: Queue[T] = {
+      mirror()
+      new QueueImpl(leading.tail, trailing)
+    }
+
+    def enqueue[U >: T](e: U): Queue[U] = new QueueImpl[U](leading, e :: trailing)
 
     override def toString: String = {
       "Queue<" + leading.mkString(",") + trailing.reverse.mkString(",") + ">"
     }
   }
+
 }
