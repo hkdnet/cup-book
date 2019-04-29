2ce2328657c6a25ac2e232f43d6e78f63fe489d3
hkdnet <satoko.itse@gmail.com>
2019-04-29 23:40:06 +0900

    mirror

--- src/main/scala/net/hkdnet/structure/Queue.scala
+++ src/main/scala/net/hkdnet/structure/Queue.scala
@@ -4,10 +4,22 @@ class Queue[T](private val r: List[T], private val s: List[T]) {
   def this(list: List[T]) {
     this(list.take(list.size / 2), list.drop(list.size / 2).reverse)
   }
-  def head: T = r.head
-  def tail: Queue[T] = new Queue(r.tail, s)
+
+  // TODO: rebalance...?
+  private def mirror: Queue[T] = {
+    if (r.isEmpty) {
+      new Queue(s.reverse, Nil)
+    } else {
+      this
+    }
+  }
+
+  def head: T = mirror.r.head
+  def tail: Queue[T] = {
+    val q = mirror
+    new Queue(q.r.tail, q.s)
+  }
   def enqueue(e: T): Queue[T] = new Queue[T](r, e::s)
-  // TODO: rebalance...
 
   override def toString: String = {
     "Queue<" + r.mkString(",") + s.reverse.mkString(",") + ">"
