da0f0981a24b248d4806edc7af77e8fddc931e36
hkdnet <satoko.itse@gmail.com>
2019-04-29 23:49:14 +0900

    Use trait and make impl private

--- src/main/scala/net/hkdnet/structure/Queue.scala
+++ src/main/scala/net/hkdnet/structure/Queue.scala
@@ -1,31 +1,38 @@
 package net.hkdnet.structure
 
-class Queue[T] private (private val leading: List[T], private val trailing: List[T]) {
-  def this(list: List[T]) {
-    this(list.take(list.size / 2), list.drop(list.size / 2).reverse)
-  }
-
-  // TODO: rebalance...?
-  private def mirror: Queue[T] = {
-    if (leading.isEmpty) {
-      new Queue(trailing.reverse, Nil)
-    } else {
-      this
-    }
-  }
-
-  def head: T = mirror.leading.head
-  def tail: Queue[T] = {
-    val q = mirror
-    new Queue(q.leading.tail, q.trailing)
-  }
-  def enqueue(e: T): Queue[T] = new Queue[T](leading, e::trailing)
-
-  override def toString: String = {
-    "Queue<" + leading.mkString(",") + trailing.reverse.mkString(",") + ">"
-  }
+trait Queue[T] {
+  def head: T
+  def tail: Queue[T]
+  def enqueue(e: T): Queue[T]
 }
 
+
 object Queue {
-  def apply[T](xs: T*) = new Queue[T](xs.toList, Nil)
+  def apply[T](xs: T*): Queue[T] = new QueueImpl[T](xs.toList)
+
+  private class QueueImpl[T] private (private val leading: List[T], private val trailing: List[T]) extends Queue[T] {
+    def this(list: List[T]) {
+      this(list.take(list.size / 2), list.drop(list.size / 2).reverse)
+    }
+
+    // TODO: rebalance...?
+    private def mirror: QueueImpl[T] = {
+      if (leading.isEmpty) {
+        new QueueImpl(trailing.reverse, Nil)
+      } else {
+        this
+      }
+    }
+
+    def head: T = mirror.leading.head
+    def tail: Queue[T] = {
+      val q = mirror
+      new QueueImpl(q.leading.tail, q.trailing)
+    }
+    def enqueue(e: T): Queue[T] = new QueueImpl[T](leading, e::trailing)
+
+    override def toString: String = {
+      "Queue<" + leading.mkString(",") + trailing.reverse.mkString(",") + ">"
+    }
+  }
 }
