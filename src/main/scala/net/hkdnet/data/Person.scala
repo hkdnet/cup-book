919dbd06e5ad6572507511f10a7032ccb100e74f
hkdnet <satoko.itse@gmail.com>
2019-04-30 01:37:34 +0900

    OrderedMergeSort

---
+++ src/main/scala/net/hkdnet/data/Person.scala
@@ -0,0 +1,12 @@
+package net.hkdnet.data
+
+class Person(val firstName: String, val lastName: String)
+  extends Ordered[Person] {
+  def compare(that: Person): Int = {
+    val lastNameComparison = lastName.compareToIgnoreCase(that.lastName)
+    if (lastNameComparison != 0) lastNameComparison
+    else firstName.compareToIgnoreCase(that.firstName)
+  }
+
+  override def toString: String = firstName + " " + lastName
+}
--- src/main/scala/net/hkdnet/main.scala
+++ src/main/scala/net/hkdnet/main.scala
@@ -25,6 +25,27 @@ object Spiral {
   }
 }
 
+object OrderedMergeSort {
+  def orderedMergeSort[T <: Ordered[T]](xs: List[T]): List[T] = {
+    def merge(xs: List[T], ys: List[T]): List[T] = {
+      (xs, ys) match {
+        case (Nil, _) => ys
+        case (_, Nil) => xs
+        case (x::xs1, y::ys1) =>
+          if (x < y) x :: merge(xs1, ys)
+          else y:: merge(xs, ys1)
+      }
+    }
+
+    val n = xs.length / 2
+    if (n == 0) xs
+    else {
+      val (ys, zs) = xs.splitAt(n)
+      merge(orderedMergeSort(ys), orderedMergeSort(zs))
+    }
+  }
+}
+
 object Main {
   def main(args: Array[String]): Unit = args match {
     case Array() => println("USAGE: action")
@@ -59,6 +80,19 @@ object Main {
       println(q.enqueue(1))
       println(q.enqueue(1).enqueue(2))
       println(Queue(1, 2))
+    }
+    case Array("sort") => {
+      import net.hkdnet.data.Person
+
+      val persons = List(
+        new Person("Miki", "Hoshii"),
+        new Person("Ritsuko", "Akiduki"),
+        new Person("Makoto", "Kikuchi"),
+      )
+
+      println(persons)
+      println("---")
+      println(OrderedMergeSort.orderedMergeSort(persons))
     }
   }
 }
