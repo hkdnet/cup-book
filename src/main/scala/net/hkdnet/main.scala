a5f6d1d6a5e7b5c68511eaecc4922a23a7669d3a
hkdnet <satoko.itse@gmail.com>
2019-05-04 18:45:33 +0900

    Split

--- src/main/scala/net/hkdnet/main.scala
+++ src/main/scala/net/hkdnet/main.scala
@@ -4,27 +4,6 @@ import net.hkdnet.numeric._
 import net.hkdnet.structure._
 import net.hkdnet.app._
 
-object OrderedMergeSort {
-  def orderedMergeSort[T <: Ordered[T]](xs: List[T]): List[T] = {
-    def merge(xs: List[T], ys: List[T]): List[T] = {
-      (xs, ys) match {
-        case (Nil, _) => ys
-        case (_, Nil) => xs
-        case (x::xs1, y::ys1) =>
-          if (x < y) x :: merge(xs1, ys)
-          else y:: merge(xs, ys1)
-      }
-    }
-
-    val n = xs.length / 2
-    if (n == 0) xs
-    else {
-      val (ys, zs) = xs.splitAt(n)
-      merge(orderedMergeSort(ys), orderedMergeSort(zs))
-    }
-  }
-}
-
 object Main {
   def main(args: Array[String]): Unit = args match {
     case Array() => println("USAGE: action")
@@ -60,18 +39,8 @@ object Main {
       println(q.enqueue(1).enqueue(2))
       println(Queue(1, 2))
     }
-    case Array("sort") => {
-      import net.hkdnet.data.Person
-
-      val persons = List(
-        new Person("Miki", "Hoshii"),
-        new Person("Ritsuko", "Akiduki"),
-        new Person("Makoto", "Kikuchi"),
-      )
-
-      println(persons)
-      println("---")
-      println(OrderedMergeSort.orderedMergeSort(persons))
+    case Array("sorter") => {
+      OrderedMergeSort.examples
     }
   }
 }
