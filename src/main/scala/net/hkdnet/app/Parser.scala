d8d5d6cbd6b245682e61fe7c865483e963bc09f8
hkdnet <satoko.itse@gmail.com>
2019-05-16 06:37:46 +0900

    Show Expr

--- src/main/scala/net/hkdnet/app/Parser.scala
+++ src/main/scala/net/hkdnet/app/Parser.scala
@@ -1,9 +1,17 @@
 package net.hkdnet.app
 
+import net.hkdnet.numeric.Expr
 import net.hkdnet.parser._
+
 
 object Parser {
   def expr(input: String) = {
-    println(ExprParser.parseAll(ExprParser.expr, input))
+    val result = ExprParser.parseAll(ExprParser.expr, input) map (e => Expr.format(e))
+
+    if (result.successful) {
+      println(result.get)
+    } else {
+      println(result)
+    }
   }
 }
--- src/main/scala/net/hkdnet/parser/ExprParser.scala
+++ src/main/scala/net/hkdnet/parser/ExprParser.scala
@@ -1,9 +1,22 @@
 package net.hkdnet.parser
 
 import scala.util.parsing.combinator._
+import net.hkdnet.numeric._
 
 object ExprParser extends JavaTokenParsers {
-  def expr: Parser[Any] = term~rep("+"~term | "-"~term)
-  def term: Parser[Any] = factor~rep("*"~factor | "/"~factor)
-  def factor: Parser[Any] = floatingPointNumber | "("~expr~")"
+  def expr: Parser[Expr] = term~rep("+"~term | "-"~term) ^^ {
+    case t~xs => {
+      xs.foldLeft(t) {
+        case(lhs, op~rhs) => BinOp(op, lhs, rhs)
+      }
+    }
+  }
+  def term: Parser[Expr] = factor~rep("*"~factor | "/"~factor) ^^ {
+    case f ~ xs => {
+      xs.foldLeft(f) {
+        case (lhs, op~rhs) => BinOp(op, lhs, rhs)
+      }
+    }
+  }
+  def factor: Parser[Expr] = floatingPointNumber^^(e => Number(e.toDouble)) | "("~>expr<~")"
 }
