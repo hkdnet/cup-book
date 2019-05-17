86c8fef733dbf9d68db8847758ae15224b6a1158
hkdnet <satoko.itse@gmail.com>
2019-05-17 18:27:50 +0900

    Add Model to control our app state

--- src/main/scala/net/hkdnet/gui/SpreadSheet.scala
+++ src/main/scala/net/hkdnet/gui/SpreadSheet.scala
@@ -2,12 +2,34 @@ package net.hkdnet.gui
 
 import scala.swing._
 
+class Model(val height: Int, val width: Int) {
+  case class Cell(row: Int, column: Int)
+  val cells = Array.ofDim[Cell](height, width)
+  for (i <- 0 until height; j <- 0 until width)
+    cells(i)(j) = new Cell(i, j)
+}
+
 class SpreadSheet(val height: Int, val width: Int) extends ScrollPane {
+  val cellModel = new Model(height, width)
+  import cellModel._
+
   val table = new Table(height, width) {
     rowHeight = 25
     autoResizeMode = Table.AutoResizeMode.Off
     showGrid = true
     gridColor = new Color(150, 150, 150)
+
+    override def rendererComponent(isSelected: Boolean, focused: Boolean, row: Int, column: Int): Component = {
+      if(hasFocus) new TextField(userData(row, column))
+      else
+        new Label(cells(row)(column).toString) {
+          xAlignment= Alignment.Right
+        }
+    }
+    def userData(row: Int, column: Int): String  = {
+      val v = this(row, column)
+      if (v == null) "" else v.toString
+    }
   }
 
   val rowHeader =
