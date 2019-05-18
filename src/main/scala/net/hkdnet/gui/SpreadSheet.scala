5a773a5566cf8616e24d0b0b1ef05737f7d1c684
hkdnet <satoko.itse@gmail.com>
2019-05-18 19:23:25 +0900

    Split

--- src/main/scala/net/hkdnet/gui/SpreadSheet.scala
+++ src/main/scala/net/hkdnet/gui/SpreadSheet.scala
@@ -1,67 +1,10 @@
 package net.hkdnet.gui
 
+import net.hkdnet.data.{Empty, Formula}
+import net.hkdnet.parser.FormulaParsers
+
 import scala.swing._
 import scala.swing.event.TableUpdated
-import scala.util.parsing.combinator.RegexParsers
-
-trait Formula
-
-case class Coord(row: Int, column: Int) extends Formula {
-  override def toString: String = ('A' + column).toChar.toString + row
-}
-
-case class Range(c1: Coord, c2: Coord) extends Formula {
-  override def toString: String = c1 + ":" + c2
-}
-
-case class Number(value: Double) extends Formula {
-  override def toString: String = value.toString
-}
-
-case class Textual(value: String) extends Formula {
-  override def toString: String = value
-}
-
-case class Application(function: String, arguments: List[Formula]) extends Formula {
-  override def toString: String = function + arguments.mkString("(", ",", ")")
-}
-
-object Empty extends Textual("")
-
-object FormulaParser extends RegexParsers {
-  def ident: Parser[String] = """[a-zA-Z_]\w*""".r
-
-  def decimal: Parser[String] = """-?\d+(\.\d*)?""".r
-
-  def cell: Parser[Coord] =
-    """[A-Za-z]\d+""".r ^^ { e =>
-      val column = e.charAt(0).toUpper - 'A'
-      val row = e.substring(1).toInt
-      Coord(row, column)
-    }
-
-  def range: Parser[Range] =
-    cell ~ ":" ~ cell ^^ { case c1 ~ ":" ~ c2 => Range(c1, c2) }
-
-  def number: Parser[Number] = decimal ^^ (e => Number(e.toDouble))
-
-  def application: Parser[Application] =
-    ident ~ "(" ~ repsep(expr, ",") ~ ")" ^^ {
-      case f ~ "(" ~ params ~ ")" => Application(f, params)
-    }
-
-  def expr: Parser[Formula] = range | cell | number | application
-
-  def textual: Parser[Textual] = "" ^^ (_=>Empty) | """[^=].*""".r ^^ Textual
-
-  def formula: Parser[Formula] = number | textual | "=" ~> expr
-
-  def parse(input: String): Formula =
-    parseAll(formula, input) match {
-      case Success(result, _) => result
-      case f: NoSuccess => Textual("[" + f.msg + "]")
-    }
-}
 
 class Model(val height: Int, val width: Int) {
 
@@ -103,7 +46,7 @@ class SpreadSheet(val height: Int, val width: Int) extends ScrollPane {
     reactions += {
       case TableUpdated(table, rows, column) => {
         for (row <- rows)
-          cells(row)(column).formula = FormulaParser.parse(userData(row, column))
+          cells(row)(column).formula = FormulaParsers.parse(userData(row, column))
       }
     }
   }
---
+++ src/main/scala/net/hkdnet/parser/FormulaParsers.scala
@@ -0,0 +1,39 @@
+package net.hkdnet.parser
+
+import scala.util.parsing.combinator._
+import net.hkdnet.data.{Formula,Coord, Range, Number, Application, Textual, Empty}
+
+object FormulaParsers extends RegexParsers {
+  def ident: Parser[String] = """[a-zA-Z_]\w*""".r
+
+  def decimal: Parser[String] = """-?\d+(\.\d*)?""".r
+
+  def cell: Parser[Coord] =
+    """[A-Za-z]\d+""".r ^^ { e =>
+      val column = e.charAt(0).toUpper - 'A'
+      val row = e.substring(1).toInt
+      Coord(row, column)
+    }
+
+  def range: Parser[Range] =
+    cell ~ ":" ~ cell ^^ { case c1 ~ ":" ~ c2 => Range(c1, c2) }
+
+  def number: Parser[Number] = decimal ^^ (e => Number(e.toDouble))
+
+  def application: Parser[Application] =
+    ident ~ "(" ~ repsep(expr, ",") ~ ")" ^^ {
+      case f ~ "(" ~ params ~ ")" => Application(f, params)
+    }
+
+  def expr: Parser[Formula] = range | cell | number | application
+
+  def textual: Parser[Textual] = "" ^^ (_=>Empty) | """[^=].*""".r ^^ Textual
+
+  def formula: Parser[Formula] = number | textual | "=" ~> expr
+
+  def parse(input: String): Formula =
+    parseAll(formula, input) match {
+      case Success(result, _) => result
+      case f: NoSuccess => Textual("[" + f.msg + "]")
+    }
+}
